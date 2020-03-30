1b322dfd128577ae0f8b675a2474c75069fca3e9
nikithakamurthy <nikithakamurthy@gmail.com>
2020-03-30 10:44:18 +0530

    .

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/movie-pagination'));
+app.use(express.static(__dirname + '/dist/pagination-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/movie-pagination/index.html'));
+res.sendFile(path.join(__dirname+'/dist/pagination-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
